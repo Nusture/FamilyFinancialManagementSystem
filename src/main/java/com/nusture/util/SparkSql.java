9c7be39160dfd9639872206fd312e5f37e11c2a9
Nusture <xubofei98@163.com>
2022-02-25 16:51:21 +0800

    修复一些参数错误

--- src/main/java/com/nusture/util/SparkSql.java
+++ src/main/java/com/nusture/util/SparkSql.java
@@ -8,6 +8,9 @@ import org.apache.spark.sql.SparkSession;
 import java.util.Properties;
 
 public class SparkSql {
+    static String url = "jdbc:mysql://localhost:3306/ffms?useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull&useSSL=false";
+    static String password = "989023Xbf";
+    static String username = "root";
     public Dataset<Row> selectTable(String table, String sql){
         SparkConf conf = new SparkConf()
                 .set("spark.testing.memory","2147480000")
@@ -24,11 +27,11 @@ public class SparkSql {
         //设置sparkSession数据连接
         Dataset dataSet = sparkSession.read()
                 .format("jdbc")
-                .option("url","jdbc:mysql://localhost:3306/ffms?useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull&useSSL=false")
+                .option("url",url)
                 .option("dbtable",table)
                 .option("driver","com.mysql.cj.jdbc.Driver")
-                .option("user","root")
-                .option("password","root")
+                .option("user",username)
+                .option("password",password)
                 .load();
         //注册临时表后才能进行select等操作，必需，否则not found in database 'default'
         dataSet.createOrReplaceTempView(table);
@@ -36,11 +39,9 @@ public class SparkSql {
         return sqlDF;
     }
     public void save(Dataset<Row> dataset,String tableName){
-
-        String url = "jdbc:mysql://localhost:3306/ffms?useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull&useSSL=false";
         Properties connectionProperties = new Properties();
-        connectionProperties.put("user","root");
-        connectionProperties.put("password","root");
+        connectionProperties.put("user",username);
+        connectionProperties.put("password",password);
         connectionProperties.put("driver","com.mysql.cj.jdbc.Driver");
 
         dataset.write().mode("append").jdbc(url,tableName,connectionProperties);
@@ -48,10 +49,9 @@ public class SparkSql {
     }
     public void saveOverwrite(Dataset<Row> dataset,String tableName){
 
-        String url = "jdbc:mysql://localhost:3306/ffms?useUnicode=true&characterEncoding=utf-8&zeroDateTimeBehavior=convertToNull&useSSL=false";
         Properties connectionProperties = new Properties();
-        connectionProperties.put("user","root");
-        connectionProperties.put("password","root");
+        connectionProperties.put("user",username);
+        connectionProperties.put("password",password);
         connectionProperties.put("driver","com.mysql.cj.jdbc.Driver");
 
         dataset.write().mode("overwrite").jdbc(url,tableName,connectionProperties);
